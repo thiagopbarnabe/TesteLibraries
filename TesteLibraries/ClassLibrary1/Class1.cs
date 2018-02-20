aa7cbe466463febef196762713d96a24dae08288
thiagopbarnabe <thiagopbarnabe@gmail.com>
2018-02-20 11:34:52 -0300

    First Commit

---
+++ TesteLibraries/ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void teste()
+        {
+
+
+            //var start = new StartOptions {
+            //    ServerFactory=
+            //};
+            Trace.TraceInformation(typeof(Microsoft.Owin.Host.HttpListener.OwinHttpListener).FullName);
+            WebApp.Start<Class2>("http://localhost:8124");
+        }
+        
+    }
+}
---
+++ TesteLibraries/ClassLibrary1/Class2.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class2
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var type = typeof(Microsoft.Owin.Host.HttpListener.OwinServerFactory);
+
+            
+            
+        }
+    }
+}
